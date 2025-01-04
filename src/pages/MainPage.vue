93267e42d484c0eb7a26d89d0f68be5763d12cd3
Gregory Pereverzev <boot762@gmail.com>
2025-01-04 15:21:32 +0300

    feat: implement saving of unplayed words to localStorage

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -4,6 +4,11 @@ import { ref } from 'vue'
 import type { GameStatus } from '@/models'
 import wordsData from '@/assets/json/top1000.json'
 import { useScoreStore } from '@/stores/score'
+import {
+  saveWordsToLocalStorage,
+  getWordsFromLocalStorage,
+  updateWordsInLocalStorage,
+} from '@/utils/localStorageManager'
 
 const gameStatus = ref<GameStatus>('notStarted')
 const timer = ref<number>(0)
@@ -13,18 +18,42 @@ const playedWords = ref<string[]>([])
 const currentWord = ref<string | null>(null)
 const scoreStore = useScoreStore()
 
-const showRandomWord = () => {
+/**
+ * Initializes the words array by either retrieving it from localStorage or
+ * creating a new one from the wordsData array.
+ */
+const initializeWords = (): void => {
+  const storedWords = getWordsFromLocalStorage()
+
+  if (!storedWords.length) {
+    words.value = wordsData.words.map(({ english }) => english)
+    saveWordsToLocalStorage(words.value)
+
+    return
+  }
+
+  words.value = storedWords
+}
+
+/**
+ * Selects a random word from the words array and sets it as the current word.
+ */
+const showRandomWord = (): void => {
   const randomIndex = Math.floor(Math.random() * words.value.length)
   currentWord.value = words.value[randomIndex]
 }
 
-const startGame = () => {
-  try {
-    words.value = wordsData.words.map((word) => word.english)
-  } catch (error) {
-    console.error('Failed to load words:', error)
-  }
-
+/**
+ * Resets the game state and starts a new game.
+ *
+ * Initializes the words array from local storage or the wordsData array,
+ * resets the score store, sets the game status to 'inProgress', and starts
+ * a 10 second timer interval. When the timer reaches 0, the interval is
+ * cleared. After starting the timer, a random word is selected and set as
+ * the current word.
+ */
+const startGame = (): void => {
+  initializeWords()
   scoreStore.reset()
   gameStatus.value = 'inProgress'
   timer.value = 10
@@ -39,11 +68,27 @@ const startGame = () => {
   showRandomWord()
 }
 
-const filterOutCurrentWord = () => {
+/**
+ * Removes the current word from the words array.
+ *
+ * This is done by creating a new array from the words array that excludes
+ * the current word. The new array is then set as the new words array.
+ */
+const filterOutCurrentWord = (): void => {
   words.value = words.value.filter((word) => word !== currentWord.value)
 }
 
-const processWord = (isSuccess: boolean) => {
+/**
+ * Processes the current word based on the user's action.
+ *
+ * If the user was successful, it increments the guessedWords score in the
+ * score store. If not, it increments the skippedWords score. It then filters
+ * the current word out of the words array, updates the playedWords array,
+ * checks if the game is over, and shows a new random word.
+ *
+ * @param {boolean} isSuccess Whether the user was successful or not.
+ */
+const processWord = (isSuccess: boolean): void => {
   filterOutCurrentWord()
 
   if (isSuccess) {
@@ -57,24 +102,61 @@ const processWord = (isSuccess: boolean) => {
   showRandomWord()
 }
 
-const updatePlayedWords = () => {
+/**
+ * Updates the playedWords array by adding the current word to it.
+ *
+ * If there is a current word, it is pushed to the playedWords array
+ * and then the currentWord is set to null.
+ */
+
+const updatePlayedWords = (): void => {
   if (currentWord.value) {
     playedWords.value.push(currentWord.value)
     currentWord.value = null
   }
 }
 
-const checkIsGameOver = () => {
+/**
+ * Checks if the game is over based on the timer value and the length of the
+ * words array.
+ *
+ * If the timer has reached 0 or if the words array is empty, the gameStatus
+ * is set to 'finished'.
+ */
+const checkIsGameOver = (): void => {
   if (timer.value === 0 || words.value.length === 0) {
     gameStatus.value = 'finished'
   }
 }
-const skipCurrentWord = () => {
+
+/**
+ * Skips the current word by processing it as unsuccessful.
+ *
+ * This function calls processWord with a false value indicating that the
+ * current word was not guessed successfully. It increments the skippedWords
+ * score, updates the playedWords array, checks if the game is over, and
+ * selects a new random word to display.
+ */
+const skipCurrentWord = (): void => {
   processWord(false)
 }
 
-const handleWordSuccess = () => {
+/**
+ * Processes the current word as a successful guess.
+ *
+ * This function calls processWord with a true value indicating that the
+ * current word was guessed successfully. It increments the guessedWords
+ * score, updates the playedWords array, checks if the game is over, and
+ * selects a new random word to display.
+ */
+const handleWordSuccess = (): void => {
   processWord(true)
+}
+
+const restartGame = (): void => {
+  updateWordsInLocalStorage(playedWords.value)
+  playedWords.value = []
+  startGame()
 }
 </script>
 
@@ -130,7 +212,7 @@ const handleWordSuccess = () => {
         <button
           type="button"
           class="text-white bg-violet-500 hover:bg-violet-400 py-4 px-6 text-2xl rounded-lg"
-          @click="startGame"
+          @click="restartGame"
         >
           Play Again
         </button>
