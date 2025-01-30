79db4e25db2a038be20b4cc2aff6b0dfc3bb6ce0
Gregory Pereverzev <boot762@gmail.com>
2025-01-30 15:12:32 +0300

    commit fot testing CI/CD pipeline

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,6 @@ import MainPage from '@/pages/MainPage.vue'
 </script>
 
 <template>
-  <div class="text-white">Test commit</div>
+  <div class="text-white">Test commit 2</div>
   <MainPage />
 </template>
